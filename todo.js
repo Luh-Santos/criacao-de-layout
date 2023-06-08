5562b89f04707f55d3c0a1bb5de731878b427341
Luh <luisasasilva19@gmail.com>
2023-06-08 00:53:18 -0300

    adicionar e excluir items

---
+++ todo.js
@@ -0,0 +1,56 @@
+const inputDia = document.getElementById("todo-dia-input");
+const listDia = document.getElementById("todo-dia-ul");
+const inputNoite = document.getElementById("todo-noite-input");
+const listNoite = document.getElementById("todo-noite-ul");
+
+function addTask() {
+  if (inputDia.value === "") {
+    alert("Você deve escrever uma tarefa!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputDia.value;
+    listDia.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "X";
+    li.appendChild(span);
+  }
+  inputDia.value = "";
+}
+
+listDia.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
+
+function addTaskNoite() {
+  if (inputNoite.value === "") {
+    alert("Você deve escrever uma tarefa!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputNoite.value;
+    listNoite.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "X";
+    li.appendChild(span);
+  }
+  inputDia.value = "";
+}
+
+listNoite.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
